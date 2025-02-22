cbb503cc73c8ba94017fa312098649636454f558
Gabriel Maso <maso.gabriel97@gmail.com>
2025-02-22 14:02:17 -0300

    Variables en python

---
+++ variables.py
@@ -0,0 +1,10 @@
+nombre = "Juan"
+edad = 25
+altura = 1.75
+es_estudiante = True
+
+# ¡ Normas para nombrar Variables !
+# Los nombres de las variables solo pueden contener letras (a-z, A-Z), números (0-9) y guiones bajos (_). No pueden comenzar con un número.
+# No se pueden utilizar palabras clave reservadas de Python como nombres de variables (por ejemplo, if, else, for, while, etc.).
+# Python distingue entre mayúsculas y minúsculas, por lo que nombre y Nombre son variables diferentes.
+# Se recomienda utilizar nombres descriptivos para las variables, que indiquen claramente su propósito: nombre, edad, total_ventas, etc.
