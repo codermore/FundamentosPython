b4afeef104d63654ae8e28ca6a2cde98fa473f85
Gabriel Maso <maso.gabriel97@gmail.com>
2025-02-25 20:18:32 -0300

    buenas practicas en las estructuras

--- estructurasdecontrol.py
+++ estructurasdecontrol.py
@@ -12,3 +12,43 @@ elif calificacion >= 70:
 
 else:
    print ("Necesita mejorar")
+
+
+# Otra forma mas eficiente de esctructurar elif encadenados es la siguiente:
+
+esUsuarioActivo = True
+esUsuarioSubscripto = True
+esUsuarioPago = True
+
+def mala_estructura ():   
+   if esUsuarioActivo:
+      if esUsuarioSubscripto:
+         if esUsuarioPago:
+            print("respuesta satisfactoria")
+         else:
+            print("no es usuario pago")
+      else:
+         print("no es usuario subscripto")
+   else:
+      print("no es usuario activo")
+
+mala_estructura()
+
+#Ahora de una manera mas legible y escalable (?)
+
+def buena_estructura():
+   if not esUsuarioActivo:
+      return print("no es usuario activo")
+   
+   if not esUsuarioSubscripto:
+      return print ("no es usuario subscripto")
+   
+   if not esUsuarioPago:
+      return print ("no es usuario pago")
+   
+   return print("respuesta satisfactoria")
+
+buena_estructura()
+
+       
+            
